5e0ac92623a221b06b3922401f47f991db7e90c2
BallyB <ajeanne25@gmail.com>
2016-05-18 15:30:29 +0200

    aa

--- Vue.java
+++ Vue.java
@@ -1,4 +1,6 @@
 package mvc;
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 
 public class Vue extends JFrame{
@@ -6,7 +8,16 @@ public class Vue extends JFrame{
 	private VueAffichage vA;
 	private VueEntree vE;
 	private VueProgressBar vPB;
+	
 	public Vue(){
+		
+		vA = new VueAffichage();
+		vE = new VueEntree();
+		vPB = new VueProgressBar();
+		
+		add(vA.getContenu(),BorderLayout.CENTER);
+		add(vE.getChamp(), BorderLayout.NORTH);
+		
 		
 		
 		
--- VueEntree.java
+++ VueEntree.java
@@ -1,24 +1,45 @@
 package mvc;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JTextField;
 
-public class VueEntree extends Observable implements Observer{
+public class VueEntree extends Observable implements Observer, ActionListener{
 
 	
 
 	public JTextField champ;
 	public VueEntree(){
 		this.champ = new JTextField();
-		
+		this.champ.setText("40");
+		this.champ.setFont(new Font("Times New Roman", Font.BOLD, 24));
+		this.champ.setColumns(10);
+		this.champ.addActionListener(this);
+
+	}
+
+	public JTextField getChamp() {
+		return champ;
+	}
+
+	public void setChamp(JTextField champ) {
+		this.champ = champ;
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
 		// TODO Auto-generated method stub
-		
+		champ.setText(Integer.toString((Integer)arg));
+
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+		this.setChanged();
+		this.notifyObservers(champ.getText());
+	} 
+
 	
 }
